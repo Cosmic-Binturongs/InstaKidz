7b36045a6251b3c5156a0f70c98a5e834ad5095c
Guangjian Bao <guangjianbao@gmail.com>
2022-05-02 16:39:20 -0400

    chang to OneToOne relationship

--- back-end/stagram/models.py
+++ back-end/stagram/models.py
@@ -1,21 +1,11 @@
 from django.db import models
-
+from django.contrib.auth import User
 # Create your models here.
-
-
-class User(models.Model):
-    username = models.CharField(max_length=128)
-    password = models.CharField(max_length=25)
-    email = models.CharField(max_length=50)
-
-    def __str__(self):
-        return f'{self.username}-{self.email}'
 
 
 class Profile(models.Model):
     bio = models.TextField()
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='user')
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.bio
