3289f5d99180e14383994aefe10336e66288b512
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-05-03 12:59:30 -0400

    React frontend components and files created

---
+++ front-end/components/Post.js
@@ -0,0 +1,6 @@
+/* The post component will be for each post itself, it should only render three items per post, all posts will be rendered back to back in the timeline page
+ 
+ - Username (poster)
+ - Img 
+ - Caption with this format (User: Caption)
+
