f1d8f7e8368614044638d4397c0aa14f36899a6e
Ainsley Brundage <abrundage95@gmail.com>
2022-05-03 14:09:40 -0400

    Removed json import

--- back-end/photo_app/views.py
+++ back-end/photo_app/views.py
@@ -2,7 +2,6 @@ from rest_framework import viewsets
 
 from .serializers import PhotoSerializer
 from .models import Photo
-import json
 
 class PhotoViewSet(viewsets.ModelViewSet):
     queryset = Photo.objects.all()
