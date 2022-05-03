71d2b4726b8df28b0ef351e09c0feb5ca17e2980
Nasim Akbor <akbor.nasim@gmail.com>
2022-05-03 12:06:37 -0400

    todo

--- back-end/slothstagram/urls.py
+++ back-end/slothstagram/urls.py
@@ -14,8 +14,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from rest_framework import routers
+from stagram.views import UserViewSets
+
+router = routers.DefaultRouter()
+router.register(f'user', UserViewSets)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('stagram.urls')),
+
 ]
