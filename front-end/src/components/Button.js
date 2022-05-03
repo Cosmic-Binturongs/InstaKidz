9b67c6703875d1f11dcc6652f3b2b4c56104ffd9
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-05-03 16:15:49 -0400

    created button component

--- front-end/src/components/Button.js
+++ front-end/src/components/Button.js
@@ -1,7 +1,22 @@
-/* We need buttons with multiple states ...
+/* We need buttons to reflect props ...
     - Login (landing page)
     - Sign up (landing page)
     - New Post (timeline page)
     - Log Out (timeline page)
     - View User Post (timeline page)
 */
+
+import React from 'react'
+
+function Button(props) {
+
+  return (
+    <div className={props.className}>
+      <button onClick={props.onClick}>
+        {props.name}
+      </button>
+    </div>
+  )
+}
+
+export default Button
