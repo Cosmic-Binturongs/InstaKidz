6fe75bd184522f902c7a4beb785e0567497ced26
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-05-04 12:12:27 -0400

    added get token to urls

--- back-end/photo_app/urls.py
+++ back-end/photo_app/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
-from .views import UserViewSet, PhotoViewSet, login_view
+from .views import UserViewSet, PhotoViewSet, login_view, get_csrf_token
 
 router = routers.DefaultRouter()
 router.register(r'user', PhotoViewSet)
@@ -9,5 +9,6 @@ router.register(r'user', UserViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', login_view)
+    path('login/', login_view),
+    path('get-csrf-token/', get_csrf_token)
 ]
--- back-end/photo_app/views.py
+++ back-end/photo_app/views.py
@@ -16,6 +16,15 @@ import json
 from django.contrib.auth import authenticate, login
 from django.http import JsonResponse
 from django.views.decorators.csrf import require_POST
+from django.views.decorators.csrf import ensure_csrf_cookie
+from django.middleware.csrf import get_token
+
+
+@ensure_csrf_cookie
+def get_csrf_token(request):
+  json_response = JsonResponse({'ok': True})
+  json_response['X-CSRFToken'] = get_token(request)
+  return json_response
 
 @require_POST
 def login_view(request):
