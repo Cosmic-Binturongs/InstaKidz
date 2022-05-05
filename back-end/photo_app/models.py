513f6b9d7bab1911f11a225f4f2b6b42fe236714
Nasim Akbor <akbor.nasim@gmail.com>
2022-05-05 12:36:40 -0400

    formatting fixed

--- back-end/photo_app/models.py
+++ back-end/photo_app/models.py
@@ -4,8 +4,7 @@ from django.contrib.auth.models import User
 
 class Photo(models.Model):
     caption = models.TextField(default=None, blank=True, null=True)
-    user = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name='photos')
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
 
     def __str__(self):
         return f'{self.url}'
@@ -16,8 +15,7 @@ class Profile(models.Model):
     pronouns = models.CharField(max_length=25)
     parents_email = models.EmailField()
     birthday = models.DateField()
-    user = models.OneToOneField(
-        User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.bio
