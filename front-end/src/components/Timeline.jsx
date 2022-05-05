d80e63425a0b180180f38118bcc221b5681ce6ce
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-05-05 14:49:49 -0400

    timeline first pass

---
+++ front-end/src/components/Timeline.jsx
@@ -0,0 +1,25 @@
+/* The Timeline component will be rendering all the Post component back to back, and the navbar will be sitting at the top of the viewport, this component should be worked on after Post component is created
+ */
+
+import React from 'react'
+import Navbar from './Navbar.jsx'
+import Post from './Post.jsx'
+
+function Timeline(props) {
+  //functions go hyah
+
+
+  return (
+    <div className='timeline'>
+      <Navbar />
+      <div className='Post'>
+        {props.map((post, i) => (
+          <Post key={i} post={post} />
+        ))}
+      </div>
+    </div>
+  )
+}
+
+export default Timeline
+
