5aacb1f9c9d6e2cbc5a6d3dd181e8034946976fd
Andrew Manuel <andrewmanuel@Andrews-MacBook-Pro.local>
2022-05-05 16:53:50 -0400

    login is working tg

--- back-end/photo_app/urls.py
+++ back-end/photo_app/urls.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 
-from .views import UserViewSet, PhotoViewSet, login_view, logout_view, session_view, whoami_view, signup_view, GetCSRFToken
+from .views import CheckAuthView, UserViewSet, PhotoViewSet, login_view, logout_view, session_view, whoami_view, signup_view, GetCSRFToken
 
 router = routers.DefaultRouter()
 router.register(f'photo', PhotoViewSet)
@@ -12,9 +12,10 @@ urlpatterns = [
     path('', include(router.urls)),
     path('sign-up/', signup_view.as_view()),
     path('csrf-cookie/', GetCSRFToken.as_view()),
-    path('login/', login_view, name='user-login'),
-    path('logout/', logout_view, name='user-logout'),
-    path('session/', session_view, name='user-session'),
-    path('whoami/', whoami_view, name='user-whoami'),
+    path('login/', login_view.as_view()),
+    path('logout/', logout_view.as_view()),
+    path('test/', CheckAuthView.as_view())
+    # path('session/', session_view, name='user-session'),
+    # path('whoami/', whoami_view, name='user-whoami'),
 
 ]
--- back-end/photo_app/views.py
+++ back-end/photo_app/views.py
@@ -20,6 +20,21 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import ensure_csrf_cookie, csrf_protect
 from django.views.decorators.http import require_POST
 
+
+@method_decorator(csrf_protect, name='dispatch')
+class CheckAuthView(APIView):
+  def get(self, request, format=None):
+    try: 
+      isAuthenticated = User.is_authenticated
+
+      if isAuthenticated:
+        return JsonResponse({'isAuthenticated': 'success'})
+      else:
+        return JsonResponse({'isAuthenticated': 'error'})
+    except: 
+      return JsonResponse({'error': 'something went wrong when checking authentication status'})
+
+
 @method_decorator(csrf_protect, name='dispatch')
 class signup_view(APIView):
   permission_classes = (permissions.AllowAny, )
@@ -31,27 +46,29 @@ class signup_view(APIView):
     password = data['password']
     re_password = data['re_password']
 
-    if password == re_password:
-      if User.objects.filter(username=username).exists():
-        return JsonResponse({'error': 'Sorry pal, that username is already taken'})
+    try: 
+      if password == re_password:   
+          if User.objects.filter(username=username).exists():
+            return JsonResponse({'error': 'Sorry pal, that username is already taken'})
+          else: 
+            if len(password) < 6:
+              return JsonResponse({'error': 'that password is too short buddy. It needs to be longer than 6 characters'})
+            else:
+              user = User.objects.create_user(username=username, password=password)
+
+              user.save()
+
+              user = User.objects.get(id=user.id)
+
+              user_profile = Profile(user=user, bio='', pronouns='', parents_email='', birthday='')
+              user_profile.save()
+
+              return JsonResponse({'success': 'user created successfully'})
+        
       else: 
-        if len(password) < 6:
-          return JsonResponse({'error': 'that password is too short buddy. It needs to be longer than 6 characters'})
-        else:
-          user = User.objects.create_user(username=username, password=password)
-
-          user.save()
-
-          user = User.objects.get(username=username)
-
-          user_profile = Profile(user, bio='', pronouns='', parents_email='', birthday='')
-          user_profile.save()
-
-          return JsonResponse({'success': 'user created successfully'})
-
-    else: 
-      return JsonResponse({'error': 'the passwords do not match'})
-
+        return JsonResponse({'error': 'the passwords do not match'})
+    except: 
+        return JsonResponse({'error': 'something went wrong when signing up'})
 
 @method_decorator(ensure_csrf_cookie, name='dispatch')
 class GetCSRFToken(APIView):
@@ -60,30 +77,60 @@ class GetCSRFToken(APIView):
   def get(self, request, format=None):
     return JsonResponse({'success': 'CSRF cookie set'})
 
-@require_POST
-def login_view(request):
-    data = json.loads(request.body)
-    username = data.get('username')
-    password = data.get('password')
-
-    if username is None or password is None:
-        return JsonResponse({'detail': 'Please provide username and password.'}, status=400)
-
-    user = authenticate(username=username, password=password)
-
-    if user is None:
-        return JsonResponse({'detail': 'Invalid credentials.'}, status=400)
-
-    login(request, user)
-    return JsonResponse({'detail': 'Successfully logged in.'})
 
 
-def logout_view(request):
-    if not request.user.is_authenticated:
-        return JsonResponse({'detail': 'You\'re not logged in.'}, status=400)
+@method_decorator(csrf_protect, name='dispatch')
+class login_view(APIView):
+    permission_classes = (permissions.AllowAny, )
 
-    logout(request)
-    return JsonResponse({'detail': 'Successfully logged out.'})
+    def post(self, request, format=None):
+      data = self.request.data 
+      username = data['username']
+      password = data['password']
+
+      try: 
+        user = authenticate(username=username, password=password)
+
+        if user is not None: 
+          login(request, user)
+          return JsonResponse({'success': 'User authenticated', 'username': username})
+        else: 
+          return JsonResponse({'error': 'error authenticating'})
+      except:
+        return JsonResponse({'error': 'something went wrong when logging in'})
+
+class logout_view(APIView): 
+  def post(self, request, format=None):
+    try: 
+      logout(request)
+      return JsonResponse({'success': 'logged out'})
+    except: 
+            return JsonResponse({'error': 'something went wrong when logging out'})
+
+
+# @require_POST
+# def login_view(request):
+#     data = json.loads(request.body)
+#     username = data.get('username')
+#     password = data.get('password')
+
+#     if username is None or password is None:
+#         return JsonResponse({'detail': 'Please provide username and password.'}, status=400)
+
+
+#     if user is None:
+#         return JsonResponse({'detail': 'Invalid credentials.'}, status=400)
+
+#     login(request, user)
+#     return JsonResponse({'detail': 'Successfully logged in.'})
+
+
+# def logout_view(request):
+#     if not request.user.is_authenticated:
+#         return JsonResponse({'detail': 'You\'re not logged in.'}, status=400)
+
+#     logout(request)
+#     return JsonResponse({'detail': 'Successfully logged out.'})
 
 
 #@ensure_csrf_cookie
