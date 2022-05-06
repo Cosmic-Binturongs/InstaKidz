6536e80d664eb3bc5b65e167d3903659bd7304f2
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-05-06 14:39:51 -0400

    Push errors

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,13 +2,22 @@ import Access from './components/Access.jsx';
 import {useState} from 'react';
 
 function App() {
+
+    /* Session state will determine between logged in or not */
+
     let [session, setSession] = useState(false);
+    const testBtn = (e) => {
+        setSession(sesh => !sesh)
+    }
 
     return (
         <div className="App">
-        {!session ? <Access/>
+        {!session ? <Access />
             : <h1>You are sign in</h1>
         }
+
+        { /* Test Button just to switch between session states */ }
+        <button onClick={testBtn}>Test Sign In</button>
 
         </div>
     );
