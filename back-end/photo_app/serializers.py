2cd6fd6b624ab9c243cbeaaad9eb69d9cfd92de1
Ainsley Brundage <abrundage95@gmail.com>
2022-05-06 14:50:09 -0400

    save merge changes

--- back-end/photo_app/serializers.py
+++ back-end/photo_app/serializers.py
@@ -2,11 +2,6 @@ from rest_framework import serializers
 from .models import Photo
 from django.contrib.auth.models import User
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> abe9b36c3c7519a1134d19fb40c88434c0d7b131
 class PhotoSerializer(serializers.ModelSerializer):
     user = PhotoUserSerializer()
     class Meta:
@@ -14,11 +9,7 @@ class PhotoSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
-<<<<<<< HEAD
     class Meta:
-=======
-     class Meta:
->>>>>>> abe9b36c3c7519a1134d19fb40c88434c0d7b131
         model = User
         fields = ['id', 'username', 'password']
         extra_kwargs = {'password': {'write_only': True, 'required': True}}
