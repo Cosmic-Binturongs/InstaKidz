6b1fe97f16849d2604c1949d366fcf371c0c91aa
t <git@example.com>
2022-05-06 14:42:44 -0400

    testing s3

--- back-end/photo_app/serializers.py
+++ back-end/photo_app/serializers.py
@@ -2,12 +2,14 @@ from rest_framework import serializers
 from .models import Photo
 from django.contrib.auth.models import User
 
+
 class PhotoSerializer(serializers.ModelSerializer):
     class Meta:
         model = Photo
         fields = '__all__'
 
-class UserSerializer(serializers.ModelSerializer): 
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['id', 'username', 'password']
