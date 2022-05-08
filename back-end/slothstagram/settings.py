db4e28b67fc88fa7795afb7769b8327e66a65c43
Emil Terrero <emil@Emils-MacBook-Pro.local>
2022-05-07 21:31:37 -0400

    most of upload photo infrustructure -- not working yet DO NOT MERGE

--- back-end/slothstagram/settings.py
+++ back-end/slothstagram/settings.py
@@ -86,6 +86,7 @@ WSGI_APPLICATION = 'slothstagram.wsgi.application'
 CORS_ALLOWED_ORIGINS = [
     "https://www.instakidz.com",
     "http://localhost:8080",
+    "http://localhost:3000"
 ]
 
 # Database
