d752549b5bc8230d0d8b633999d1b4747e9a5256
James Thompson <jamesthompson@Jamess-Mac-mini-4.local>
2022-06-20 13:48:15 -0400

    changes made on pres-day to models and views. New migration altering photo_photo

--- back-end/photo_app/models.py
+++ back-end/photo_app/models.py
@@ -4,8 +4,9 @@ from django.contrib.auth.models import User
 
 class Photo(models.Model):
     caption = models.TextField(default=None, blank=True, null=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='photos')
-    photo = models.ImageField(max_length=128)
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='photos')
+    photo = models.ImageField(max_length=1024)
 
     def __str__(self):
         return f'{self.caption}'
@@ -16,7 +17,8 @@ class Profile(models.Model):
     pronouns = models.CharField(max_length=25)
     parents_email = models.EmailField()
     birthday = models.DateField()
-    user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
+    user = models.OneToOneField(
+        User, on_delete=models.CASCADE, primary_key=True)
 
     def __str__(self):
         return self.bio
